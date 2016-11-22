122934268ae178f9d0299507309b9cc11db50bd2
Georgi Spasov <georgi.spasov@outlook.com>
2016-11-22 22:23:56 +0200

    Fill the matrix

--- FillTheMatrix/FillTheMatrix.cs
+++ FillTheMatrix/FillTheMatrix.cs
@@ -130,6 +130,7 @@ namespace FillTheMatrix
             for (int r = 0; r < arr.GetLength(0); r++)
             {
                 for (int c = 0; c < arr.GetLength(1); c++)
+
                 {
                     Console.Write("{0}", arr[r, c]);
                     if (c != arr.GetLength(1) - 1)
