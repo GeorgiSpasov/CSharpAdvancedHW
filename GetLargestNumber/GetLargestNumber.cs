4ec10be70e48c47196f4ace5381ad9d8b9e947fc
Georgi Spasov <georgi.spasov@outlook.com>
2016-11-24 12:20:32 +0200

    Get largest number

---
+++ GetLargestNumber/GetLargestNumber.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace GetLargestNumber
+{
+    class GetLargestNumber
+    {
+        public static void GetMax()
+        {
+
+            string list = Console.ReadLine();
+            //char[] separators = new char[] { ',', ' ', '.' };
+            //int[] nums = list.Split(separators, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            int[] nums = list.Split(new char[] { ',', ' ', '.' }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+            int a = nums[0];
+            int b = nums[1];
+            int c = nums[2];
+
+            Console.WriteLine((a>b && a>c)?a:((b>c)?b:c));
+        }
+
+        static void Main(string[] args)
+        {
+            GetMax();
+        }
+    }
+}
