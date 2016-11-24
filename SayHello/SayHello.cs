06df135d61f0e88236b7968ff795492d3062ba15
Georgi Spasov <georgi.spasov@outlook.com>
2016-11-24 10:23:14 +0200

    Say Hello

---
+++ SayHello/SayHello.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SayHello
+{
+    class SayHello
+    {
+        public static void SayHi(String name)
+        {
+            Console.WriteLine("Hello, {0}!", name);
+        }
+        static void Main(string[] args)
+        {
+            String name = Console.ReadLine();
+            SayHi(name);
+        }
+    }
+}
