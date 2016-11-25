bdb680090e1b347515aa2d889493952829f3deca
Georgi Spasov <georgi.spasov@outlook.com>
2016-11-25 23:23:14 +0200

    Decimal to hexadecimal

---
+++ DecimalToHexadecimal/DecimalToHexadecimal.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DecimalToHexadecimal
+{
+    class DecimalToHexadecimal
+    {
+        static void Main(string[] args)
+        {
+            long n = long.Parse(Console.ReadLine());
+            Console.WriteLine(n.ToString("X"));
+        }
+    }
+}
