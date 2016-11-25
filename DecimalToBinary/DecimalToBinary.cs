ab4691137e161a673ddfe96f2e9d10b70bfa9d76
Georgi Spasov <georgi.spasov@outlook.com>
2016-11-25 23:05:27 +0200

    Decimal to binary

---
+++ DecimalToBinary/DecimalToBinary.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DecimalToBinary
+{
+    class DecimalToBinary
+    {
+        static void Main(string[] args)
+        {
+            long n = long.Parse(Console.ReadLine());
+            Console.WriteLine(Convert.ToString(n, 2));
+        }
+    }
+}
