1dd0182d647f308f3b9aa72a95957ab904efbb3a
Georgi Spasov <georgi.spasov@outlook.com>
2016-11-25 23:27:18 +0200

    Hexadecimal to decimal

---
+++ HexadecimalToDecimal/HexadecimalToDecimal.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HexadecimalToDecimal
+{
+    class HexadecimalToDecimal
+    {
+        static void Main(string[] args)
+        {
+            string hex = Console.ReadLine();
+            Console.WriteLine(Convert.ToUInt64(hex, 16).ToString());
+        }
+    }
+}
