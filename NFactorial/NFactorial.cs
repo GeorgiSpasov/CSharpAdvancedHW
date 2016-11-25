448e2dfca6b1a0575796b065ffa8017791283a75
Georgi Spasov <georgi.spasov@outlook.com>
2016-11-25 22:00:45 +0200

    N Factorial

---
+++ NFactorial/NFactorial.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Numerics;
+
+namespace NFactorial
+{
+    class NFactorial
+    {
+        static BigInteger FactorialNCalc(int n)
+        {
+            BigInteger result = n;
+            if (n != 0)
+            {
+                for (int i = 1; i < n; i++)
+                {
+                    result = result * i;
+                }
+            }
+            else
+            {
+                result = 1;
+            }
+            return result;
+        }
+
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine(FactorialNCalc(n));
+        }
+    }
+}
