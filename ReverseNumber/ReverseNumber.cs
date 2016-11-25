5ee868fb4cd152c8f5bb281ea664245c4d61b2ef
Georgi Spasov <georgi.spasov@outlook.com>
2016-11-25 20:17:48 +0200

    Reverse number

---
+++ ReverseNumber/ReverseNumber.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ReverseNumber
+{
+    class ReverseNumber
+    {
+        static void NumberReverser()
+        {
+            string number = Console.ReadLine();
+            //char[] reversedNumber = new char[number.Length];
+            for (int i = number.Length - 1, j = 0; i >= 0; i--, j++)
+            {
+                //reversedNumber[j] = number[i];
+                Console.Write(number[i]);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            NumberReverser();
+        }
+    }
+}
