a15ac5c7ca765bd83fbc49eb1252fd740d7a3078
Georgi Spasov <georgi.spasov@outlook.com>
2016-11-25 20:12:07 +0200

    Appearance count

---
+++ AppearanceCount/AppearanceCount.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+namespace AppearanceCount
+{
+    class AppearanceCount
+    {
+        static void IntCounter()
+        {
+            int counter = 0;
+            int n = int.Parse(Console.ReadLine());
+            //int[] intArray = new int[n];
+            string input = Console.ReadLine();
+            int[] intArray = input.Split(new char[] { ',', ' ', '.' }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+            int x = int.Parse(Console.ReadLine());
+
+
+            for (int i = 0; i < intArray.Length; i++)
+            {
+                if (intArray[i] == x)
+                {
+                    counter++;
+                }
+            }
+            Console.WriteLine(counter);
+        }
+
+        static void Main(string[] args)
+        {
+            IntCounter();
+        }
+    }
+}
