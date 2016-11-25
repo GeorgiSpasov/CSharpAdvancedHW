6d88e170713d6d0e9da1c09fdd3895bf7d5e299f
Georgi Spasov <georgi.spasov@outlook.com>
2016-11-25 23:19:23 +0200

    Binary to decimal

---
+++ BinaryToDecimal/BinaryToDecimal.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace BinaryToDecimal
+{
+    class BinaryToDecimal
+    {
+        static void Main(string[] args)
+        {
+            string binary = Console.ReadLine();
+            Console.WriteLine(Convert.ToUInt64(binary, 2).ToString());
+        }
+    }
+}
