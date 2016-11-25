b7f9d0b067ac6aeeee576c16d4c77eca10fdb87a
Georgi Spasov <georgi.spasov@outlook.com>
2016-11-25 23:38:45 +0200

    Binary to hexadecimal

---
+++ BinaryToHexadecimal/BinaryToHexadecimal.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace BinaryToHexadecimal
+{
+    class BinaryToHexadecimal
+    {
+        static void Main(string[] args)
+        {
+            string binary = Console.ReadLine();
+            string hex = Convert.ToInt64(binary, 2).ToString("X");
+            Console.WriteLine(hex);
+        }
+    }
+}
