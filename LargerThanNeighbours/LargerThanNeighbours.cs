d85b509fdb90c8f7efea7738fabdd08fed3f4790
Georgi Spasov <georgi.spasov@outlook.com>
2016-11-25 20:13:48 +0200

    Larger than neighbours

---
+++ LargerThanNeighbours/LargerThanNeighbours.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace LargerThanNeighbours
+{
+    class LargerThanNeighbours
+    {
+        static void LargerCounter()
+        {
+            int counter = 0;
+            int n = int.Parse(Console.ReadLine());
+            string input = Console.ReadLine();
+            int[] intArray = input.Split(new char[] { ',', ' ', '.' }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+            for (int i = 1; i < intArray.Length - 1; i++)
+            {
+                if (intArray[i - 1] < intArray[i] && intArray[i] > intArray[i + 1])
+                {
+                    counter++;
+                }
+            }
+            Console.WriteLine(counter);
+        }
+
+        static void Main(string[] args)
+        {
+            LargerCounter();
+        }
+    }
+}
