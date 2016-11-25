a7172fc7677eabc2e5a0096152ab509fe46c65a0
Georgi Spasov <georgi.spasov@outlook.com>
2016-11-25 20:10:20 +0200

    English Digit

---
+++ EnglishDigit/EnglishDigit.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace EnglishDigit
+{
+    class EnglishDigit
+    {
+        static void IntToWord()
+        {
+            string input = Console.ReadLine();
+            int lastDigit = Convert.ToInt32(input[input.Length - 1] - '0');
+            string[] words = { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+            Console.WriteLine(words[lastDigit]);
+        }
+
+        static void Main(string[] args)
+        {
+            IntToWord();
+        }
+    }
+}
