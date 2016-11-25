cc4fe45d27ab7cdd334dc16ac2db78acddb29da6
Georgi Spasov <georgi.spasov@outlook.com>
2016-11-25 21:58:40 +0200

    Sorting array

---
+++ SortingArray/SortingArray.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace SortingArray
+{
+    class SortingArray
+    {
+        static void ArrSorter()
+        {
+            int n = int.Parse(Console.ReadLine());
+            string input = Console.ReadLine();
+            int[] intArray = input.Split(new char[] { ',', ' ', '.' }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            Array.Sort(intArray);
+            Console.WriteLine(String.Join(" ", intArray));
+
+
+        }
+
+        static void Main(string[] args)
+        {
+            ArrSorter();
+        }
+    }
+}
