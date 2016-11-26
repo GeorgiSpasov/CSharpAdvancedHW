a1293b5ab71137cb280778a5cfe3ad15aaf08179
Georgi Spasov <georgi.spasov@outlook.com>
2016-11-26 02:09:16 +0200

    One system to any other

---
+++ OneSystemToAnyOther/OneSystemToAnyOther.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace OneSystemToAnyOther
+{
+    class OneSystemToAnyOther
+    {
+        //Turns S base number to Dec based
+        static ulong TurnS_BaseToDec(int s, string inputSbasedNumber)
+        {
+            ulong result = 0;
+            for (int i = 0; i < inputSbasedNumber.Length; i++)
+            {
+                int currentDigit;
+                if (inputSbasedNumber[i] < 58)
+                {
+                    currentDigit = inputSbasedNumber[i] - 48;
+                }
+                else
+                {
+                    currentDigit = inputSbasedNumber[i] - 55;
+                }
+                result = result * (ulong)s + (ulong)currentDigit;
+            }
+            return result;
+        }
+        //Turns Dec based number to D based number
+        static string TurnDecToBaseD(int baseD, ulong inputDec)
+        {
+            string result = "";
+            while (inputDec > 0)
+            {
+                string currentDigit = "";
+                if (inputDec % (ulong)baseD < 10)
+                {
+                    currentDigit = ((inputDec % (ulong)baseD)).ToString();
+                }
+                else
+                {
+                    currentDigit = ((char)((inputDec % (ulong)baseD) + 55)).ToString();
+                }
+                result = currentDigit + result;
+                inputDec = inputDec / (ulong)baseD;
+            }
+            if (result == "")
+            {
+                result = "0";
+            }
+            return result;
+        }
+        static void Main()
+        {
+            int baseS = int.Parse(Console.ReadLine());
+            string inputNumber = Console.ReadLine();
+            int baseD = int.Parse(Console.ReadLine());
+            Console.WriteLine(TurnDecToBaseD(baseD, TurnS_BaseToDec(baseS, inputNumber)));
+        }
+    }
+}
