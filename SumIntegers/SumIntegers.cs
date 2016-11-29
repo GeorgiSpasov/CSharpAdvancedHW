50305abf3948cbf497475f35bd40496db175468f
Georgi Spasov <georgi.spasov@outlook.com>
2016-11-29 21:26:53 +0200

    Sum integers

---
+++ SumIntegers/SumIntegers.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+namespace SumIntegers
+{
+    class SumIntegers
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int[] intArray = input.Split(new char[] { ',', ' ', '.' }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            Console.WriteLine(intArray.Sum());
+        }
+    }
+}
