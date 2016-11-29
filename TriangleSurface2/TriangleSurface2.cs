d52552284d11b4a9fdd4c4e4269aba816b6a7256
Georgi Spasov <georgi.spasov@outlook.com>
2016-11-29 21:10:58 +0200

    Triangle surface by two sides and an angle

--- TriangleSurface2/TriangleSurface2.cs
+++ TriangleSurface2/TriangleSurface2.cs
@@ -1,15 +1,65 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace TriangleSurface2
 {
     class TriangleSurface2
     {
+        private class Triangle
+        {
+            private double a;
+            private double b;
+            private double angle;
+
+            public double A
+            {
+                get
+                {
+                    return a;
+                }
+                set
+                {
+                    a = value;
+                }
+            }
+
+            public double B
+            {
+                get
+                {
+                    return b;
+                }
+                set
+                {
+                    b = value;
+                }
+            }
+
+            public double Angle
+            {
+                get
+                {
+                    return angle;
+                }
+                set
+                {
+                    angle = value;
+                }
+            }
+
+            public void CalcSurface()
+            {
+                Console.WriteLine("{0:f2}", this.a * this.b * Math.Sin(Math.PI * angle / 180.0) / 2);
+            }
+
+        }
+
         static void Main(string[] args)
         {
+            Triangle test = new Triangle();
+            test.A = double.Parse(Console.ReadLine());
+            test.B = double.Parse(Console.ReadLine());
+            test.Angle = double.Parse(Console.ReadLine());
+            test.CalcSurface();
         }
     }
 }
