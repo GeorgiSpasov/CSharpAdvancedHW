9f2da905f5cdd7e053b75bdaa5c275dfe571e220
Georgi Spasov <georgi.spasov@outlook.com>
2016-11-29 16:54:30 +0200

    Leap year

---
+++ LeapYear/LeapYear.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace LeapYear
+{
+    class LeapYear
+    {
+        static void Main(string[] args)
+        {
+            int input = int.Parse(Console.ReadLine());
+            Console.WriteLine(DateTime.IsLeapYear(input)? "Leap": "Common");
+        }
+    }
+}
