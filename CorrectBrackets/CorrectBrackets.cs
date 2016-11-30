51f98b94da288cb7a4616b7be6e030288e675973
Georgi Spasov <georgi.spasov@outlook.com>
2016-11-30 22:46:28 +0200

    Correct brackets

---
+++ CorrectBrackets/CorrectBrackets.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace CorrectBrackets
+{
+    class CorrectBrackets
+    {
+        static void Main(string[] args)
+        {
+            char[] input = Console.ReadLine().ToCharArray();
+            
+            int bracket1 = 0;
+            int bracket2 = 0;
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == '(')
+                {
+                    bracket1++;
+                }
+                if (input[i] == ')')
+                {
+                    bracket2++;
+                }
+            }
+            bool correctOrNot = bracket1 == bracket2;
+            
+            Console.WriteLine(correctOrNot ? "Correct" : "Incorrect");
+        }
+    }
+}
