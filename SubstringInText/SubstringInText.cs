aaf712ad0b796d4ef95b4046d04d5a77b9e6cb42
Georgi Spasov <georgi.spasov@outlook.com>
2016-11-30 22:39:38 +0200

    Sub-string in text

---
+++ SubstringInText/SubstringInText.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace SubstringInText
+{
+    class SubstringInText
+    {
+        static void Main(string[] args)
+        {
+            string substring = Console.ReadLine().ToLower();
+            string input = Console.ReadLine().ToLower();
+            Console.WriteLine(SubStringInText(substring, input));
+        }
+
+        static int SubStringInText(string pattern, string inputSentence)
+        {
+            int counter = 0;
+            int i = 0;
+            while ((i = inputSentence.IndexOf(pattern, i)) != -1)
+            {
+                counter++;
+                i++;
+            }
+            return counter;
+        }
+    }
+}
