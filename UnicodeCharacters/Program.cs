a030a2bd09dfc70bbbb5fb1a1518d968eff633f2
Georgi Spasov <georgi.spasov@outlook.com>
2016-11-30 22:56:46 +0200

    Unicode characters

---
+++ UnicodeCharacters/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading;
+using System.Globalization;
+
+namespace UnicodeCharacters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
+            
+            string input = Console.ReadLine();
+           
+            for (int i = 0; i < input.Length; i++)
+            {
+                Console.Write("\\u{0:X4}", (int)input[i]);  
+            }
+            Console.WriteLine();
+        }
+    }
+}
