a62e154c2355abf22438801f5d83f34eab0b0e3e
Georgi Spasov <georgi.spasov@outlook.com>
2016-11-30 23:01:18 +0200

    Replace tags

---
+++ ReplaceТags/ReplaceТags.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace ReplaceТags
+{
+    class ReplaceТags
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string output = Regex.Replace(input, @"<a.href=""([^>]*)"">([^<]*(?:(?!</a)<[^<]*)*)</a>", "[$2]($1)");
+            Console.WriteLine(output);
+        }
+    }
+}
