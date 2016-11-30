1eb37fce47b3b60e76d859a6a9fd379599071d09
Georgi Spasov <georgi.spasov@outlook.com>
2016-11-30 22:51:12 +0200

    String length

---
+++ StringLength/StringLength.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace StringLength
+{
+    class StringLength
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            
+            char[] inputAsChar = new char[20];
+            for (int i = 0; i < inputAsChar.Length; i++)
+            {
+                if (i < input.Length)
+                {
+                    inputAsChar[i] = input[i];
+                }
+                else
+                {
+                    inputAsChar[i] = '*';
+                }
+            }
+            
+            Console.WriteLine(inputAsChar);
+        }
+    }
+}
