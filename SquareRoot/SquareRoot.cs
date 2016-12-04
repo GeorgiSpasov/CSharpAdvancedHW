81839abb353077f228c4d73a16a50d6174a33694
Georgi Spasov <georgi.spasov@outlook.com>
2016-12-04 19:42:13 +0200

    Square root

---
+++ SquareRoot/SquareRoot.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace SquareRoot
+{
+    class SquareRoot
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                double input = double.Parse(Console.ReadLine());
+                if (input < 0)
+                {
+                    Console.WriteLine("Invalid number");
+                }
+                else
+                {
+                    Console.WriteLine("{0:f3}", Math.Sqrt(input));
+                }
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Invalid number");
+            }
+            finally
+            {
+                Console.WriteLine("Good bye");
+            }
+        }
+    }
+}
